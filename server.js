c1ea703924427a6535660934b4c99aaca9d599b2
rdhanush <rdhanush329@gmail.com>
2026-06-17 15:36:22 +0530

    server changes

--- server.js
+++ server.js
@@ -1,51 +1,185 @@
 require("dotenv").config();
-require("./config/db")
-let express = require("express")
-
-let app= express();
-let http = require("http");
-let server = http.createServer(app);
-let {Server} = require("socket.io")
-let io = new Server(server,{
-    cors:{
-        origin:"*"
+ 
+require("./config/db");
+ 
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+ 
+const app = express();
+ 
+const server = http.createServer(app);
+ 
+const { Server } = require("socket.io");
+ 
+ 
+// Socket Configuration
+ 
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: [
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE"
+        ],
+        credentials: true
     }
-})
-
-let socket = require("./socket/socket");
-socket.init(io)
-
-app.use(express.json())
-app.use("/user",require("./routes/userroutes"))
-app.use("/profile",require("./routes/profileroutes"))
-app.use("/uploads",express.static("uploads"))
-app.use("/friend",require("./routes/profileroutes"))
-app.use("/connect",require("./routes/connectionrequestroutes"))
-app.use("/getting",require("./routes/chatroutes"))
-app.use("/post",require("./routes/postroutes"))
-app.use("/group",require("./routes/grouproutes"))
-app.use("/message",require("./routes/messageroutes"))
-app.use("/directmessage",require("./routes/directmessageroutes"))
-
-io.on("connection",(socket)=>{
-    console.log("user connected")
-    socket.on(
-        "joingroup",
-        (groupid)=>{
-            socket.join(groupid)
-        }
-    )
-    socket.on(
-        "joinprofile",
-        (profileid)=>{
-            socket.join(profileid)
-            console.log("joinedprofile",profileid)
-        }
-
-    )
-})
-
-
-server.listen(process.env.PORT,()=>{
-    console.log("server started");
-})
+});
+ 
+ 
+const socket = require("./socket/socket");
+ 
+socket.init(io);
+ 
+ 
+// Middlewares
+ 
+app.use(
+    cors({
+        origin:"*",
+        methods: [
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE"
+        ],
+        credentials: true
+    })
+);
+ 
+ 
+app.use(
+    express.json()
+);
+ 
+ 
+// Static Files
+ 
+app.use(
+    "/uploads",
+    express.static("uploads")
+);
+ 
+ 
+// Routes
+ 
+app.use(
+    "/user",
+    require("./routes/userroutes")
+);
+ 
+ 
+app.use(
+    "/profile",
+    require("./routes/profileroutes")
+);
+ 
+ 
+app.use(
+    "/friend",
+    require("./routes/profileroutes")
+);
+ 
+ 
+app.use(
+    "/post",
+    require("./routes/postroutes")
+);
+ 
+ 
+app.use(
+    "/group",
+    require("./routes/grouproutes")
+);
+ 
+ 
+app.use(
+    "/message",
+    require("./routes/messageroutes")
+);
+ 
+ 
+app.use(
+    "/",
+    require("./routes/directmessageroutes")
+);
+ 
+ 
+// Socket Connection
+ 
+io.on(
+    "connection",
+    (socket)=>{
+ 
+        console.log(
+            "User connected:",
+            socket.id
+        );
+ 
+ 
+        socket.on(
+            "joingroup",
+            (groupid)=>{
+ 
+                socket.join(
+                    groupid
+                );
+ 
+                console.log(
+                    "Joined group:",
+                    groupid
+                );
+ 
+            }
+        );
+ 
+ 
+        socket.on(
+            "joinprofile",
+            (profileid)=>{
+ 
+                socket.join(
+                    profileid
+                );
+ 
+                console.log(
+                    "Joined profile:",
+                    profileid
+                );
+ 
+            }
+        );
+ 
+ 
+        socket.on(
+            "disconnect",
+            ()=>{
+ 
+                console.log(
+                    "User disconnected:",
+                    socket.id
+                );
+ 
+            }
+        );
+ 
+    }
+);
+ 
+ 
+// Start Server
+ 
+const PORT =
+    process.env.PORT || 5001;
+ 
+ 
+server.listen(
+    PORT,
+    ()=>{
+        console.log(
+            `Server running on port ${PORT}`
+        );
+    }
+);
